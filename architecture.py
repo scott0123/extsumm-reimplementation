99f61504c54acdff952c81a37803d600654bcdc3
Zhengzhe Yang <jackyzz1997@outlook.com>
2020-03-25 15:55:01 -0400

    added rouge evaluation

--- architecture.py
+++ architecture.py
@@ -6,6 +6,7 @@ import numpy as np
 from torch import nn
 from torch.nn import functional as F
 from torch.nn.utils.rnn import pad_sequence, pad_packed_sequence, pack_padded_sequence
+import rouge
 
 
 # The Extractive Summarization Model, re-implemented
@@ -225,8 +226,7 @@ class ExtSummModel(nn.Module):
         docs, start_ends, abstracts, labels = zip(*Xs)
         logits = self.forward(docs, start_ends)
         confidence = F.sigmoid(logits)
-        # TODO
-        return confidence.numpy()
+        return (confidence.numpy() > 0.5).float()
 
     def save(self, model_path):
         model_state = {
@@ -300,6 +300,41 @@ def load_data(data_paths, data_type="train"):
             in zip(docs, start_ends, abstracts, labels)]
 
 
+# code used from https://pypi.org/project/py-rouge/
+def prepare_results(metric, p, r, f):
+    return '\t{}:\t{}: {:5.2f}\t{}: {:5.2f}\t{}: {:5.2f}'\
+        .format(metric, 'P', 100.0 * p, 'R', 100.0 * r, 'F1', 100.0 * f)
+
+
+def print_rouge(model_path, data):
+    # get the hypothesis text
+    model = ExtSummModel.load(model_path)
+    predictions = model.predict(data)
+    docs, start_ends, abstracts, labels = zip(*data)
+    hypothesis = []
+    for doc in docs:
+        temp_hyp = []
+        for i, sentence, in enumerate(doc):
+            if predictions[i] == 1:
+                temp_hyp.append(' '.join(sentence))
+        hypothesis.append(' '.join(temp_hyp))
+
+    # set up rouge evaluator
+    evaluator = rouge.Rouge(metrics=['rouge-n', 'rouge-l', 'rouge-w'],
+                            max_n=4,
+                            limit_length=True,
+                            length_limit=200,
+                            length_limit_type='words',
+                            apply_avg=True,
+                            apply_best=False,
+                            alpha=0.5,  # Default F1_score
+                            weight_factor=1.2,
+                            stemming=True)
+    scores = evaluator.get_scores(hypothesis, list(abstracts))
+    for metric, results in sorted(scores.items(), key=lambda x: x[0]):
+        print(prepare_results(metric, results['p'], results['r'], results['f']))
+
+
 def test_forward():
     model = ExtSummModel()
     example_batch = [[[1, 2], [0, 7, 3], [5, 6, 7]], [[5, 3], [6, 7], [2], [3, 4, 5, 6]]]  # TODO
@@ -323,8 +358,15 @@ def test_with_data():
     # TODO: Shuffle the data
 
 
+def test_rouge():
+    data_paths = ("arxiv/inputs/", "arxiv/human-abstracts/", "arxiv/labels/")
+    train_set = load_data(data_paths, data_type="train")
+    print_rouge("", train_set)
+
+
 def main():
-    test_with_data()
+    # test_with_data()
+    test_rouge()
 
 
 if __name__ == "__main__":
